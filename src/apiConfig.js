fa603c8658da8d42e02833a2b7d0de05a7434b0f
Aalqkhan <khanalqama8980@gmail.com>
2026-03-26 13:45:25 +0530

    Update API URL for AWS deployment

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = `http://${window.location.hostname}:8080`;
+const API_BASE_URL = process.env.REACT_APP_API_URL || `http://${window.location.hostname}:8080`;
 export default API_BASE_URL;
